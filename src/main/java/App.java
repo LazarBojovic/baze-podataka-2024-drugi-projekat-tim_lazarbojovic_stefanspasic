3408cef716c9331d11b33a0a2f4dc0309f1f0b3e
LazarBojovic <lbojovic13522rn@raf.rs>
2024-06-02 14:42:22 +0200

    baza, login strana(konekcija)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,14 @@
 
-import Udruzenje.view.MainView;
+import planeta.model.utility.JDBCUtils;
+import planeta.view.MainView;
 import javafx.application.Application;
 import javafx.stage.Stage;
+
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        //JDBCUtils.connect();
+        JDBCUtils.connect();
         stage = new MainView();
         stage.show();
-    }}
+    }
+}
---
+++ src/main/java/planeta/view/Planeta.java
@@ -0,0 +1,5 @@
+package planeta.view;
+
+public class Planeta {
+
+}
